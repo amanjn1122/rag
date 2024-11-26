b02565286e568ab39afd3325adfff19e7f79ac8f
amanjn1122 <115038153+amanjn1122@users.noreply.github.com>
2024-11-26 12:30:06 +0530

    Delete pages/types directory

--- pages/types/user.ts
+++
@@ -1,7 +0,0 @@
-// types/user.ts (optional, but a good practice to separate types)
-export interface User {
-    name: string;
-    email: string;
-    image?: string; // Optional property for user image (if needed)
-  }
-  
