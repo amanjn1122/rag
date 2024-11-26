3c8479c9e72140087082a4fbf7b1394f9a3e5597
amanjn1122 <115038153+amanjn1122@users.noreply.github.com>
2024-11-26 15:40:31 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
 // pages/index.tsx
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import { useState } from 'react';
+// import { useRouter } from 'next/router';
+// import Cookies from 'js-cookie';
 
 const HomePage = () => {
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    // Check if the user is authenticated by looking for the JWT in cookies
-    const token = Cookies.get('token'); // Get token from cookies
+  // useEffect(() => {
+  //   // Check if the user is authenticated by looking for the JWT in cookies
+  //   const token = Cookies.get('token'); // Get token from cookies
 
-    if (token) {
-      // Redirect to the dashboard if the user is authenticated
-      router.push('/dashboard');
-    } else {
-      // Redirect to the login page if not authenticated
-      router.push('/login');
-    }
-    setLoading(false);
-  }, [router]);
+  //   if (token) {
+  //     // Redirect to the dashboard if the user is authenticated
+  //     router.push('/dashboard');
+  //   } else {
+  //     // Redirect to the login page if not authenticated
+  //     router.push('/login');
+  //   }
+  //   setLoading(false);
+  // }, [router]);
 
   if (loading) {
     // Optionally show a loading screen or spinner while checking authentication
